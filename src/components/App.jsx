e470aa5e449ed866b4a5a13671e8a1836c75d5d9
Iryna Furhala <irynafurhala@gmail.com>
2022-12-20 18:14:30 +0200

    add localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,23 @@ import { Form } from "./Form/Form"
 
 export class App extends React.Component {
   state = {
-    contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+    contacts: [],
     name: '',
     number: '',
     filter: ''
   }
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    this.setState({contacts: JSON.parse(contacts)})
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
   onSubmit = () => {
     let willAdd = true;
-
     this.state.contacts.map(contact => {
       if (contact.name === this.state.name) {
         return willAdd = false;
